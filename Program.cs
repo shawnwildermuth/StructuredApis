91a345b01c6406ae1d4d530bbb52763e5977204a
Shawn Wildermuth <shawn@wildermuth.com>
2021-11-11 12:36:27 -0500

    Update for before Demo

--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using StructuredApis.Data;
-using StructuredApis.Data.Entities;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
--- Usings.cs
+++ Usings.cs
@@ -3,4 +3,6 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
-
+global using Microsoft.EntityFrameworkCore;
+global using StructuredApis.Data;
+global using StructuredApis.Data.Entities;
